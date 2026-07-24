66b079a79e8b52fec48534cc973fbe76d16c2fa4
ashikabaskaran24-web <ashikabaskaran24@gmail.com>
2026-07-24 12:24:03 +0530

    switch prg

--- basicprg/Switch1.java
+++ basicprg/Switch1.java
@@ -26,22 +26,28 @@ public class Switch1{
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
-        System.out.println("Enter a number (1 to 3):");
-        int n = s.nextInt();
-
-        switch (n) {
+        System.out.println("1.add 2.sub 3.mul 4.div 5.remainder Enter your choice:");
+        int c=s.nextInt();
+        int a = s.nextInt();
+        int b = s.nextInt();
+        switch (c) {
             case 1:
-                System.out.println("Monday");
+                System.out.println("add:"+(a+b));
                 break;
 
             case 2:
-                System.out.println("Tuesday");
+                System.out.println("sub:"+(a-b));
                 break;
 
             case 3:
-                System.out.println("Wednesday");
+                System.out.println("mul:"+(a*b));
                 break;
-
+            case 4:
+                System.out.println("div:"+(a/b));
+                break;
+            case 5:
+                System.out.println("rem:"+(a%b));
+                break;
             default:
                 System.out.println("Invalid number");
         }
