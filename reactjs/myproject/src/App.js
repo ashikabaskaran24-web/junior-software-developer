c23d1f3f366d1075f83839174df32db8537d2ab2
ashikabaskaran24-web <ashikabaskaran24@gmail.com>
2026-08-19 14:17:55 +0530

    reactjs

---
+++ reactjs/myproject/src/App.js
@@ -0,0 +1,172 @@
+ function App()
+{
+  var arr=[10,20,30,40,50,6-0]
+  return (
+    <div>
+        <h1>{arr[0]}</h1>
+        <h1>{arr[1]}</h1>
+        <h1>{arr[2]}</h1>
+        <h1>{arr[3]}</h1>
+    </div>
+  )
+}
+export default App
+// function App()
+// {
+//   var avg=65
+//   var res="fail"
+//   return (
+//     <div>
+//       <h1>average:{avg}</h1>
+//         <h1>{(res==="pass" &&((avg>=85 && "outstanding")||(avg>=75 && "excellent")||(avg>=65 &&"very good")||(avg>=55&&"good")||"fair"))||"no grade"}</h1>
+//     </div>
+//   )
+// }
+// import React from 'react'
+// function App()
+// {
+//   var avg=65
+//   return (
+//     <div>
+//       <h1>average:{avg}</h1>
+//         <h1>{avg} is {(avg>=85 && "outstanding")||(avg>=75 && "excellent")||(avg>=65 &&"very good")||(avg>=55&&"good")||"fair"}</h1>
+//     </div>
+//   )
+// }
+// import React from 'react'
+// function App()
+// {
+//   var m1=0
+//   return (
+//     <div>
+//       <h1>number:{m1}</h1>
+//         <h1>{m1} is {(m1===0 && " zero")||(m1>0 && "positive")||"negative"}</h1>
+//     </div>
+//   )
+// }
+// export default App
+// import React from 'react'
+// function App()
+// {
+//   var m1=2004
+//   return (
+//     <div>
+//       <h1>number:{m1}</h1>
+//         <h1>{m1} is {m1%4===0 && " leap year"||"not leap"}</h1>
+//     </div>
+//   )
+// }
+// export default App
+// import React from 'react'
+// function App()
+// {
+//   var m1=40
+//   var m2=20;
+//   return (
+//     <div>
+//         <h1>{m1>m2 && m1||"m2 is big"}</h1>
+//     </div>
+//   )
+// }
+// export default App
+// import React from 'react'
+// function App()
+// {
+//   var mark=60
+//   return (
+//     <div>
+//         <h1>ifelse</h1>
+//         <h1>mark:{mark}</h1>
+//         <h1>{(mark>34 && "pass")||"fail"}</h1>
+//     </div>
+//   )
+// }
+// export default App
+// import React from 'react'
+// function App()
+// {
+//   var mark=10
+//   return (
+//     <div>
+//         <h1>mark:{mark}</h1>
+//         <h1>{mark>34 && "pass"}</h1>
+//         <h1>{mark<=34 && "fail"}</h1>
+//     </div>
+//   )
+// }
+// export default App
+/*import './App.css'
+import React from 'react'
+import myimg from './imagesreact/dog.jpg'
+function App() {
+  return (
+    <div>
+      <img src={myimg} width="90%" alt="not found"></img>
+    </div>
+  )
+}
+export default App
+/*var App=()=>{
+  var a=10
+  var b=20
+  var c=10
+  return(
+    <>
+    <h1>a:{a}</h1>
+    <h1>b:{b}</h1>
+    <h1>c:{c}</h1>
+    <h1>tot:{a+b+c}</h1>
+    </>
+  )
+}
+export default App;
+/*import React from "react";
+class App extends React.Component
+{
+  render(){
+    return(
+      <>
+    <h1>welcome to react js</h1>
+     <h2>welcome to react js</h2>
+      <h3>welcome to react js</h3>
+      </>
+    )
+  }
+}
+export default App;
+/*function App()
+{
+  return(
+    <>
+    <h1>welcome to react js</h1>
+     <h2>welcome to react js</h2>
+      <h3>welcome to react js</h3>
+      </>
+  )
+}
+export default App;
+/*import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;*/
